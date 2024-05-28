16cc070e87d13b1c0f36f1a59c7b3e592420bde9
Smalls141414 <ari.grady14@gmail.com>
2024-05-28 13:02:20 -0700

    Updated Stylesheet & HTML

---
+++ popup.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("button");
+button.addEventListener("click", async () => {
+  const tabIds = tabs.map(({ id }) => id);
+  if (tabIds.length) {
+    const group = await chrome.tabs.group({ tabIds });
+    await chrome.tabGroups.update(group, { title: "DOCS" });
+  }
+});
