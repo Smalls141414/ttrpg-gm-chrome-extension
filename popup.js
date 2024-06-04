298f8d0c46f6201b1ca8f2e2dec10ae98f271e53
Smalls141414 <ari.grady14@gmail.com>
2024-06-03 17:06:30 -0700

    Symbol update & Testing

--- popup.js
+++ popup.js
@@ -1,8 +1,3 @@
-const button = document.querySelector("button");
-button.addEventListener("click", async () => {
-  const tabIds = tabs.map(({ id }) => id);
-  if (tabIds.length) {
-    const group = await chrome.tabs.group({ tabIds });
-    await chrome.tabGroups.update(group, { title: "DOCS" });
-  }
-});
+function openInNewTab(url) {
+  window.open(url, '_blank').focus();
+}
