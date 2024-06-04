283ee00f3731d9817535f9e76df567cadad6b021
Smalls141414 <ari.grady14@gmail.com>
2024-06-03 17:08:12 -0700

    Altered Javascript

--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
-function openInNewTab(url) {
-  window.open(url, '_blank').focus();
-}
+const button = document.querySelector("button");
+button.addEventListener("click", async () => {
+  const tabIds = tabs.map(({ id }) => id);
+  if (tabIds.length) {
+    const group = await chrome.tabs.group({ tabIds });
+    await chrome.tabGroups.update(group, { title: "DOCS" });
+  }
+});
