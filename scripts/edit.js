6928f8a8c86045f47158f83359224b0c26718c91
Smalls141414 <ari.grady14@gmail.com>
2024-07-01 17:26:04 -0700

    Random Reward Testing

---
+++ scripts/edit.js
@@ -0,0 +1,65 @@
+/**
+TODO:
+- Check Duplicates when storing reward
+- Reward rarity
+**/
+
+const sf = document.getElementById("sourceForm").addEventListener('submit', changeSource);
+const ra = document.getElementById("rewardAdd").addEventListener('submit', addReward);
+document.getElementById("back").addEventListener("click", loadStartPage);
+
+async function changeSource(event)
+{
+	event.preventDefault();
+	let source = document.getElementById("source").value;
+	
+	// Sets spellSource variable in order to change spell source
+	await chrome.storage.local.set({ spellSource : source }).then(() => {
+		console.log("Source changed to " + source);
+	});
+}
+
+async function addReward(event)
+{
+	event.preventDefault();
+	let rewardName = document.getElementById("rewardName").value;
+	let rewardDesc = document.getElementById("rewardDesc").value;
+	
+	//Set rewardNum if rewardNum does not exist
+	await chrome.storage.local.get(["rewardNum"]).then((num) => {
+		
+		// Records what number reward is being added
+		var rewardID;
+		
+		if(typeof num.rewardNum === 'undefined')
+		{
+			chrome.storage.local.set({ rewardNum : "0" }).then(() => {
+				console.log("rewardNum set to 0");
+			});
+			
+			rewardID = "reward0"; 
+			
+		} else
+		{
+			// This line parses the line as an integer and increasing it for the rewardNum
+			rewardID = parseInt(num.rewardNum) + 1;
+			
+			chrome.storage.local.set({ rewardNum : rewardID }).then(() => {
+				console.log("rewardNum increased");
+			});
+			
+			// This line adds the term "reward" to the start so that it may be queried with other rewards in storage
+			rewardID = "reward" + rewardID;
+		}
+		
+		// Adds reward with reward number as ID - this allows for easy randomization later
+		chrome.storage.local.set({ [rewardID] : rewardName });
+		
+	});
+	
+}
+
+function loadStartPage(event)
+{
+	window.location="popup.html";
+}
---
+++ scripts/popup.js
@@ -0,0 +1,72 @@
+const sp = document.getElementById("spellForm").addEventListener('submit', findSpell);
+const na = document.getElementById("nameForm").addEventListener('submit', namesGet);
+const ra = document.getElementById("rewardForm").addEventListener('submit', rewardsGet);
+document.getElementById("settings").addEventListener("click", loadEditPage);
+
+
+/*
+	TODO: Add url verification before sending
+*/
+async function findSpell(event)
+{
+	// Prevent standard form events and get spelltext
+	event.preventDefault();
+	let spellText = document.getElementById("spellText").value;
+	let url;
+	
+	// Update text for url use: all lowercase, dashes in place of spaces, remove nonletters
+	spellText = spellText.toLowerCase();
+	spellText = spellText.replace(/[^a-z\s]/g, "");
+	spellText = spellText.replace(" ", "-");
+	
+	// Check chosen spell source
+	await chrome.storage.local.get(["spellSource"]).then((result) => {
+		console.log("Value is " + result.spellSource);
+		if(result.spellSource == "wikidot")
+		{
+			url = "http://dnd5e.wikidot.com/spell:" + spellText;
+		} else
+		{
+			url = "http://www.dndbeyond.com/spells/" + spellText;
+		}
+	});
+	
+	// Send and focus url
+	window.open(url, '_blank').focus();
+}
+
+/**
+	Names Function - Unfinished
+**/
+function namesGet(event)
+{
+	event.preventDefault();
+	let species = document.getElementById("species").value;
+	console.log(species);
+	
+}
+
+/**
+	Rewards Function - Unfinished
+**/
+async function rewardsGet(event)
+{
+	event.preventDefault();
+	let rarity = document.getElementById("rarity").value;
+	console.log(rarity);
+	
+	// RARITY IS TEMPORARILY IGNORED
+
+	await chrome.storage.local.get(["rewardNum"]).then((num) => {
+		let randReward = "reward" + (Math.floor(Math.random() * (num.rewardNum + 1)));
+		console.log(randReward);
+		chrome.storage.local.get([randReward]).then((rew) => {
+			console.log(rew[randReward]);
+		});
+	});
+}
+
+function loadEditPage(event)
+{
+	window.location="edit.html";
+}
